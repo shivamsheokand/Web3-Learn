b7c571ba2406315061f7a93f185a01e628c877e1
shivamsheokand <shivamsheokand32@gmail.com>
2025-06-29 16:21:32 +0530

    demo

---
+++ campain.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >= 0.7.0;
+
+contract Hello{
+    function hello() pure public returns(string memory){
+        return "Hello world";
+    }
+}
